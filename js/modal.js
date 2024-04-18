7c6b87bf2b966b935ed8a518ef9ac2c3d2625db0
Denis <denis-sergeev-85@inbox.ru>
2024-04-18 23:33:33 +0300

    сделано модальное окно

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,24 @@
 let headerContainer = document.querySelector(".head-btn-container");
 let headerModal = document.querySelector(".header__modal");
 let headerModalOpen = document.querySelector(".headerModalOpen");
+let contactModalOpen = document.querySelectorAll(".contact-modal-open");
+let contactModal = document.querySelector("#contactOpen");
+let contactCloseBtn = document.querySelector("#contact-close-btn");
 
 function openHeaderMenu() {
   headerContainer.classList.toggle("change");
   headerModal.classList.toggle("headerModalOpen");
+}
 
-  /*
-  if (ss.style.display === "none") {
-    headerModal.style.display = "block";
-  } else {
-    headerModal.style.display = "none";
-  }*/
+function closeContactModal() {
+  contactModal.style.display = "none";
 }
 
 headerContainer.addEventListener("click", openHeaderMenu);
+contactCloseBtn.addEventListener("click", closeContactModal);
+
+contactModalOpen.forEach((e) => {
+  e.addEventListener("click", () => {
+    contactModal.style.display = "block";
+  });
+});
