97c1f8aba1d574f597194ddf831898847e9442d6
Denis <denis-sergeev-85@inbox.ru>
2024-04-18 22:08:43 +0300

    исправлена вёрстка

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,17 @@
 let headerContainer = document.querySelector(".head-btn-container");
 let headerModal = document.querySelector(".header__modal");
+let headerModalOpen = document.querySelector(".headerModalOpen");
 
 function openHeaderMenu() {
   headerContainer.classList.toggle("change");
+  headerModal.classList.toggle("headerModalOpen");
 
-  if (headerModal.style.display === "none") {
-    headerModal.style.display = "inline";
+  /*
+  if (ss.style.display === "none") {
+    headerModal.style.display = "block";
   } else {
     headerModal.style.display = "none";
-  }
+  }*/
 }
 
 headerContainer.addEventListener("click", openHeaderMenu);
